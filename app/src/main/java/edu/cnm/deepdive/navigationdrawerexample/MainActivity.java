eb675bd417c1640be4e781c671892fa8db1e3a8d
Jan Gevaert <jan.gevaert@gmail.com>
2020-08-28 15:27:40 -0600

    RuntimeException. Emulator does not run.

--- app/src/main/java/edu/cnm/deepdive/navigationdrawerexample/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/navigationdrawerexample/MainActivity.java
@@ -1,6 +1,7 @@
 package edu.cnm.deepdive.navigationdrawerexample;
 
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -14,15 +15,10 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    BottomNavigationView navView = findViewById(R.id.nav_view);
-    // Passing each menu ID as a set of Ids because each
-    // menu should be considered as top level destinations.
-    AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-        R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-        .build();
-    NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-    NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-    NavigationUI.setupWithNavController(navView, navController);
+
+    Toolbar toolbar = findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+
   }
 
 }
